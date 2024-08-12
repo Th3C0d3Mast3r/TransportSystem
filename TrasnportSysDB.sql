2306e2abce687fec3531b14ff769b22c807e4760
Devesh A <143060274+Th3C0d3Mast3r@users.noreply.github.com>
2024-08-12 06:43:32 +0530

    Updated TrasnportSysDB.sql

--- TrasnportSysDB.sql
+++ TrasnportSysDB.sql
@@ -68,8 +68,8 @@ userPassword varchar(18)
 
 CREATE TABLE IF NOT EXISTS bookingLog(
 srNo int,
-userAadhar varchar(15),
-userPhoneNumber varchar(15),
+userAadhar varchar(15),		-- if this is in the userDatabase, only then book. Else Request Sing-Up
+userPhoneNumber varchar(15),	-- same for this
 travelMode varchar(20),
 travelVehicle enum("BUS", "CAB", "TRAIN", "PLANE", "YACHT") NOT NULL,
 vehicleNo varchar(20),
@@ -79,9 +79,53 @@ FOREIGN KEY (userAadhar) REFERENCES userDatabase(userAadhar)
 );
 
 -- This is the section where we add the base raw data in this. At least the initial one.
+INSERT INTO busRoute
+(modeType, busNo, totalCapacity, route, travelType, travelMode, travelDuration, fareAC, fareNonAc)
+values
+("BUS", "1000", 40, "Mumbai-Pune", "NON-AC", "NON-SLEEPER", 4, NULL, 350),
+("BUS", "1001", 40, "Mumbai-Pune", "NON-AC", "SLEEPER", 4, NULL, 400),
+("BUS", "1002", 40, "Mumbai-Pune", "AC", "SLEEPER", 4, 500, NULL),
+("BUS", "1003", 40, "Mumbai-Pune", "AC", "NON-SLEEPER", 4, 450, NULL);
+
+INSERT INTO cabRoute
+(modeType, cabNo, carModel, route, travelDuration, fareMini, farePrime, fareSUV)
+VALUES
+("CAB", "4565", "MINI", "Mumbai-Pune", 3, 800, NULL, NULL),
+("CAB", "4566", "PRIME", "Mumbai-Pune", 3, NULL, 950, NULL),
+("CAB", "4567", "SUV", "Mumbai-Pune", 3, NULL, NULL, 1500);
+
+
+
 
 -- This is the view part to see the basic tables and their schema
 SELECT * FROM busRoute;
 SELECT * FROM cabRoute;
 SELECT * FROM trainRoute;
 SELECT * FROM planeRoute;
+
+SELECT 				-- this is query for seeing route based travelling modes and compare price
+    route,
+    modeType,
+    travelMode, 
+    fareAC AS fare 
+FROM 
+    busRoute 
+WHERE 
+    route = 'Mumbai-Pune'
+
+UNION ALL
+
+SELECT 
+    route,
+    modeType,
+    carModel AS travelMode, 
+    CASE 
+        WHEN carModel = 'MINI' THEN fareMini 
+        WHEN carModel = 'PRIME' THEN farePrime 
+        WHEN carModel = 'SUV' THEN fareSUV 
+    END AS fare 
+FROM 
+    cabRoute 
+WHERE 
+    route = 'Mumbai-Pune';
+
